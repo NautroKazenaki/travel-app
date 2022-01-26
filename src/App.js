5d1420ad1ef7a4e7c37efaa13ea2f41c1d506785
Сергей Ашарин <kalliak2014@yandex.ru>
2022-01-26 20:23:46 +0300

    travel advisor api test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { CssBaseline, Grid} from '@material-ui/core';
 
+import {getPlacesData} from './api/index'
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
-    
+    //hook that will make state filled w/ restaraunts
+    const [places, setPlaces] = useState([])
+    //hook that will get restaraunts when we start an application
+    useEffect( () => {
+        getPlacesData()
+            .then( (data) => {
+                console.log(data)
+                setPlaces(data)
+            })
+    }, [])
+
     return (
         <>
             <CssBaseline />
---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+var options = {
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+  },
+  headers: {
+    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+    'x-rapidapi-key': 'd043d3f67bmsh5ab7e98d12fabc3p1b500bjsna5228342596f'
+  }
+};
+
+
+
+export const getPlacesData = async () => {
+    //making a get request w/ URL based on travel advisor api, and options I got from get endpoint (axios)
+    try {
+        // I got an object that contains {data: with object {data} that i need to use in my jsx to render}
+        const { data: { data }} = await axios.get(URL, options)
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
